be1b647d0eed96f9022e5ae0488fae897409932f
bagool <ioan.adrian98@gmail.com>
2022-12-07 22:05:58 +0000

    Adds day6 part1 solution

---
+++ day6/day6.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	common "aoc23/main"
+	"bufio"
+	"fmt"
+)
+
+func isMarker(text string) bool {
+
+	var flags [256]bool
+
+	for _, ascii := range text {
+		if flags[ascii] {
+			return false
+		}
+
+		flags[ascii] = true
+	}
+
+	return true
+}
+
+func main() {
+	inputFile := common.ReadTestFile("day6_input.txt")
+	scanner := bufio.NewScanner((inputFile))
+
+	scanner.Split(bufio.ScanRunes)
+
+	var crtMarkerCandidate []rune
+	index := 1
+
+	for scanner.Scan() {
+		char := scanner.Text()
+
+		crtMarkerCandidate = append(crtMarkerCandidate, rune(char[0]))
+
+		if len(crtMarkerCandidate) > 4 {
+			crtMarkerCandidate = crtMarkerCandidate[1:]
+
+			if isMarker(string(crtMarkerCandidate)) {
+				fmt.Printf("Marker after character: #%d", index)
+				break
+			}
+		}
+
+		index++
+	}
+}
